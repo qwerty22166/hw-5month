4a1688c1863bdf22b3285955595443b298ea4c97
qwerty22166 <nurjusu184@gmail.com>
2023-01-27 21:57:27 +0600

    Add

---
+++ 1/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CountPage from "./pages/countPage/CountPage";
+
+function App() {
+  return (
+    <div className="App">
+      <CountPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 1/my-app/src/pages/countPage/CountPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+
+function CountPage() {
+    const dispatch = useDispatch()
+    const count = useSelector(state => state.count)
+
+
+    const increment = () => {
+        dispatch({
+            type: "INCREMENT"
+        })
+    }
+
+    const decrement = () => {
+        dispatch({
+            type: "DECREMENT"
+        })
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+        </div>
+    );
+}
+
+export default CountPage;
